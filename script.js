ddbf0addbfd6ca0c8b8179127c67357d4165214a
gabrieloliveiramuniz-png <gabriel.oliveira.muniz@escola.pr.gov.br>
2026-09-09 17:57:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const botao = document.querySelector("#botaoMensagem");
 const mensagem = document.querySelector("#mensagemInterativa");
+botao.addEventListener("click", mostrarMensagem);
 function mostrarMensagem() {
     mensagem.textContent = 
     "Você acabou de criar uma interação com o Javascript";
